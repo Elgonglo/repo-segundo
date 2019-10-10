b407328fdd5f880ef53e56a0c9a7fbbc5677cbd6
ElGonglo <gonzalo.30812@gmail.com>
2019-10-10 14:40:54 +0200

    MVC pasar datos

---
+++ sgemp/03-HolaMundoPasarDatosALaVista/03-HolaMundoPasarDatosALaVista/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _03_HolaMundoPasarDatosALaVista.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+
+            //esto en una funcion aparte saludar que asigne a un string
+            DateTime hora = new DateTime();
+            hora = DateTime.Now;
+            if (hora.Hour < 12 && hora.Hour >7 )
+            {
+                ViewData["Hora"] = hora;
+            }
+            else if(hora.Hour > 12 && hora.Hour < 21)
+            {   
+
+            }
+            //ViewData["Nombre"] = “Fernando";	return View(); 
+            return View();
+        }
+
+    }
+   
+
+}
