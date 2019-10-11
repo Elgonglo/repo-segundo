357664a73aed7bda13f642c4ab94073068d5718e
ElGonglo <gonzalo.30812@gmail.com>
2019-10-11 14:40:25 +0200

    ejercicio layout grid

---
+++ DI/06-LayoutGridForm/06-LayoutGridForm/MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using _06_LayoutGridForm.Models;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// La plantilla de elemento Página en blanco está documentada en https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0xc0a
+
+namespace _06_LayoutGridForm
+{
+    /// <summary>
+    /// Página vacía que se puede usar de forma independiente o a la que se puede navegar dentro de un objeto Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            
+
+        }
+          
+        /// <summary>
+        /// Boton de enviar que validará todo
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnSend_Click(object sender, RoutedEventArgs e)
+        {
+            clsPersona persona = new clsPersona();
+            DateTime cumple = new DateTime();
+            persona.nombre = tbName.Text;
+            persona.apellido = tbSurname.Text;
+            
+
+            if (string.IsNullOrEmpty(persona.nombre))
+            {
+                tblNameError.Text = "Esta vacio";
+            }
+            else
+            {
+                tblNameError.Text = "";
+            }
+
+            if (string.IsNullOrEmpty(persona.apellido))
+            {
+                tblSurnameError.Text = "Esta vacio";
+            }
+            else
+            {
+                tblSurnameError.Text = "";
+            }
+
+           
+             if (string.IsNullOrEmpty(tbDate.Text))
+            {
+                tblDateError.Text = "La Fecha Esta Vacia";
+            }
+            else
+            {
+                if(DateTime.TryParse(tbDate.Text, out cumple))
+                {
+                    persona.fecha = cumple;
+                    tblDateError.Text = "";
+                }
+                else if(cumple > DateTime.Now)
+                {
+                    tblDateError.Text = "La Fecha Es Incorrecto";
+                }
+                
+            }
+        }
+    }
+}
---
+++ DI/06-LayoutGridForm/06-LayoutGridForm/Model/clsPersona.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06_LayoutGridForm.Models
+{
+    public class clsPersona
+    {
+        //atributos privados
+        private string _nombre;
+       
+        //propiedades publicas
+        public clsPersona()
+        {
+
+        }
+        public clsPersona(String nombre,String apellido,DateTime fecha)
+        {
+            _nombre = nombre;
+            this.apellido = apellido;
+            this.fecha = fecha;
+        }
+        public string nombre
+        {
+            get
+            {
+                return _nombre;
+            }
+            set
+            {
+                _nombre = value;   
+            }
+        }
+
+        public string apellido { get; set; }
+        public DateTime fecha { get; set; }
+    }
+}
