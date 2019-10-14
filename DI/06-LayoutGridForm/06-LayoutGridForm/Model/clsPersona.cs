dd516b3c4224d11d57f85a1b981aeb61735f6000
ElGonglo <gonzalo.30812@gmail.com>
2019-10-14 14:36:05 +0200

    Ejercicio relative panel DI

--- DI/06-LayoutGridForm/06-LayoutGridForm/Model/clsPersona.cs
+++ DI/06-LayoutGridForm/06-LayoutGridForm/Model/clsPersona.cs
@@ -6,32 +6,23 @@ namespace _06_LayoutGridForm.Models
 {
     public class clsPersona
     {
-        //atributos privados
-        private string _nombre;
-       
+        
+      
         //propiedades publicas
         public clsPersona()
         {
-
+            nombre = "Gonzalo";
+            apellido = "Gomez";
         }
         public clsPersona(String nombre,String apellido,DateTime fecha)
         {
-            _nombre = nombre;
+            this.nombre = nombre;
             this.apellido = apellido;
             this.fecha = fecha;
         }
-        public string nombre
-        {
-            get
-            {
-                return _nombre;
-            }
-            set
-            {
-                _nombre = value;   
-            }
-        }
+        
 
+        public string nombre { get; set; }
         public string apellido { get; set; }
         public DateTime fecha { get; set; }
     }
