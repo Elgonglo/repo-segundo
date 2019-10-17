52b44e453027cae2bb9c4de945c5278d570c0575
ElGonglo <gonzalo.30812@gmail.com>
2019-10-17 14:32:23 +0200

    MVC pasar datos de vista al controlador

---
+++ sgemp/04-PasarDatosDeLaVistaAlControlador/04-PasarDatosDeLaVistaAlControlador/Controllers/PersonasController.cs
@@ -0,0 +1,26 @@
+﻿using _04_PasarDatosDeLaVistaAlControlador.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _04_PasarDatosDeLaVistaAlControlador.Controllers
+{
+    public class PersonasController : Controller
+    {
+        // GET: Personas
+        public ActionResult Editar()//click derecho en editar y le pones edit
+        {
+            //Instanciamos un objeto de la clase persona con mis datos desde el constructor
+            clsPersona persona = new clsPersona();
+            //enviamos el objeto persona a la vista
+            return View(persona);
+        }
+        [HttpPost]
+        public ActionResult Editar(clsPersona persona)
+        {
+            return View("PersonasModificadas", persona);
+        }
+    }
+}
---
+++ sgemp/04-PasarDatosDeLaVistaAlControlador/04-PasarDatosDeLaVistaAlControlador/Models/clsPersona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04_PasarDatosDeLaVistaAlControlador.Models
+{
+    public class clsPersona
+    {
+        
+      
+        //propiedades publicas
+        public clsPersona()
+        {
+            nombre = "Gonzalo";
+            apellido = "Gomez";
+        }
+        public clsPersona(String nombre,String apellido,DateTime fecha)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.fecha = fecha;
+        }
+        
+
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public DateTime fecha { get; set; }
+    }
+}
