c1ee0945783c2aa898e2475b602c7edfd2795969
ElGonglo <gonzalo.30812@gmail.com>
2019-10-23 12:43:19 +0200

    bindingsss

---
+++ DI/12-LayoutGridFormConBinding/06-LayoutGridForm/Model/clsPersona.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _06_LayoutGridForm.Models
+{
+    public class clsPersona
+    {
+        
+      
+        //propiedades publicas
+        public clsPersona()
+        {
+            nombre = "Gonzalo";
+            apellido = "Gomez";
+        }
+        public clsPersona(String nombre,String apellido,DateTime fecha)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.fecha = fecha;
+        }
+        
+
+        public string nombre { get; set; }
+        public string apellido { get; set; }
+        public DateTime fecha { get; set; }
+    }
+}
