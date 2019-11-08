14897b20289ac7ef56282c8f6f24911087e41788
ElGonglo <gonzalo.30812@gmail.com>
2019-11-08 14:41:50 +0100

    terminar lo que queda de los botones eliminar y filtrar

---
+++ DI/13.3-Ejercicio3Tema9CommandsViewModel/13-Ejercicio3ViewModel-UI/Models/Utilidades/ListadoPersonas.cs
@@ -0,0 +1,30 @@
+﻿using _13_Ejercicio3Tema9_Entities ;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+
+namespace _13_Ejercicio3ViewModel_UI.Models.Utilidades
+{
+    public class ListadoPersonas
+    {
+        /// <summary>
+        /// Aqui añade las personas y devuelve la lista
+        /// </summary>
+        /// <returns>List<clsPersona></returns>
+        public ObservableCollection<clsPersona> ListarPersonas()
+        {
+            ObservableCollection<clsPersona> personas = new ObservableCollection<clsPersona>();
+            personas.Add(new clsPersona());
+            personas.Add(new clsPersona("Daniel","Gordillo"));
+            personas.Add(new clsPersona("Yeary","Campanario"));
+            personas.Add(new clsPersona("Pablo","Prats"));
+            personas.Add(new clsPersona("Angela","Vazquez"));
+            return personas;
+
+        }
+        
+
+    }
+}
---
+++ DI/13.3-Ejercicio3Tema9CommandsViewModel/13-Ejercicio3ViewModel-UI/ViewModels/ClsMainPageVM.cs
@@ -0,0 +1,84 @@
+﻿using _13_Ejercicio3Tema9_Entities;
+using _13_Ejercicio3ViewModel_UI.Models.Utilidades;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+
+namespace _13_Ejercicio3ViewModel_UI.ViewModels
+{
+    public class ClsMainPageVM : INotifyPropertyChanged
+    {
+        //despues se añade el notify
+
+        //Atributos clase persona
+        private clsPersona _personaSeleccionada;
+        private ObservableCollection<clsPersona> _listadoPersonas;
+
+        public ClsMainPageVM()
+        {
+            //se rellena la lista de personas
+            eliminar = new DelegateCommand(Eliminar, puedeEliminar);
+            ListadoPersonas listado = new ListadoPersonas();
+            this._listadoPersonas = listado.ListarPersonas();
+        }
+        public clsPersona PersonaSeleccionada
+        {
+
+            get {
+                return _personaSeleccionada;
+            }
+            set {
+                if (_personaSeleccionada != value)
+                {
+                    _personaSeleccionada = value;
+                    NotifyPropertyChanged("PersonaSeleccionada");
+                }
+
+            }
+        }
+        public String buscar { get; set; }
+        public ObservableCollection<clsPersona> ListadoPersonas
+        {
+            get
+            {
+                return _listadoPersonas;
+            }
+            set
+            {
+                _listadoPersonas = value;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void Eliminar()
+        {
+            ListadoPersonas.Remove(_personaSeleccionada);
+        }
+
+        public bool puedeEliminar()
+        {
+            if (PersonaSeleccionada != null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public DelegateCommand eliminar{get;set;}
+
+    }
+}
