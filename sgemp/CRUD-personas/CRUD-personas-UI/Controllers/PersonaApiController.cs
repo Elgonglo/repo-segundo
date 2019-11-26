6993242be6514d6922825e12a5a8271671955c0f
ElGonglo <gonzalo.30812@gmail.com>
2019-11-26 14:27:27 +0100

    api

--- sgemp/CRUD-personas/CRUD-personas-UI/Controllers/PersonaApiController.cs
+++ sgemp/CRUD-personas/CRUD-personas-UI/Controllers/PersonaApiController.cs
@@ -33,8 +33,9 @@ namespace CRUD_personas_UI.Controllers
         }
 
         // PUT: api/PersonaApi/5
-        public void Put(int id, [FromBody]string value)
+        public void Put(int id, [FromBody]clsPersona value)
         {
+            ClsManejadoraPersonas_BL manejadora = new ClsManejadoraPersonas_BL();
         }
 
         // DELETE: api/PersonaApi/5
