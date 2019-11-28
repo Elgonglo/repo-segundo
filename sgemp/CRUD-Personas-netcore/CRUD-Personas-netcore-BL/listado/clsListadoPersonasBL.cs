47bfb7d18e173c94998df053075bf90554200892
ElGonglo <gonzalo.30812@gmail.com>
2019-11-28 14:27:57 +0100

    sgemp netcore

---
+++ sgemp/CRUD-Personas-netcore/CRUD-Personas-netcore-BL/listado/clsListadoPersonasBL.cs
@@ -0,0 +1,20 @@
+﻿using _CRUD_Personas_netcore_DAL.listado;
+using CRUD_Personas_netcore_Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CRUD_Personas_netcore_BL.listado
+{
+    public class clsListadoPersonasBL
+    {
+        //mejor un list
+        public IEnumerable<clsPersona> ListadoPersonas()
+        {
+            //instanciar una clase listadoP
+            return ClsListadoPersonasDAL.listaPersonas();
+        }
+    }
+}
---
+++ sgemp/CRUD-Personas-netcore/CRUD-Personas-netcore-UI/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using CRUD_Personas_netcore_UI.Models;
+using CRUD_Personas_netcore_BL.listado;
+
+namespace CRUD_Personas_netcore_UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            try
+            {
+                clsListadoPersonasBL listado = new clsListadoPersonasBL();
+                return View(listado.ListadoPersonas());
+            }
+            catch (Exception e)
+            {
+                return View("Error"); //Pagina de error
+            }
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
