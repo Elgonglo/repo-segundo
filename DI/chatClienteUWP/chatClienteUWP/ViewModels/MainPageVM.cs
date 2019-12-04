9fcf4ab7e778b348fa774863af8782470b8e425d
ElGonglo <gonzalo.30812@gmail.com>
2019-12-04 14:32:59 +0100

    signal r con errores xdddd

---
+++ DI/chatClienteUWP/chatClienteUWP/ViewModels/MainPageVM.cs
@@ -0,0 +1,49 @@
+﻿using chatClienteUWP.Models;
+using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+
+namespace chatClienteUWP.ViewModels
+{
+   public class MainPageVM
+    {
+        public ObservableCollection<clsMensaje> Messages { get; set; } = new ObservableCollection<clsMensaje>();
+
+        private HubConnection conn { get; set; }
+        private IHubProxy proxy { get; set; }
+
+        //comando delegate broadcast
+
+        public MainPageVM()
+        {
+            SignalR();
+        }
+        private void SignalR()
+        {
+            conn = new HubConnection("http://<  ;your-mobile-app>.azurewebsites.net");
+            proxy = conn.CreateHubProxy("ChatHub");
+            conn.Start();
+
+            proxy.On<clsMensaje>("broadcastMessage", OnMessage);
+
+        }
+
+        ///Aqui hay errores
+        public void Broadcast(String nombre,String mensaje )
+        {
+            proxy.Invoke("Send",nombre, mensaje);
+        }
+        private async void OnMessage(String nombre,String mensaje)
+        {
+            await Windows.ApplicationModel.Core.CoreApplication.MainView.Dispatcher.RunAsync(CoreDispatcherPriority.Normal, () =>
+            {
+                Messages.Add(nombre,mensaje);
+            });
+        }
+    }
+}
---
+++ DI/chatServer/chatServer/chatHub.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace chatServer
+{
+    public class chatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+
+        //public void SendUWP(clsMensaje mensaje)
+        //{
+        //    // Call the broadcastMessage method to update clients.
+        //    Clients.All.broadcastMessage(mensaje);
+        //}
+    }
+}
